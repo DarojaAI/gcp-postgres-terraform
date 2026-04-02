f3a882fd342026cbcc2516ac37ab3bf3ab88423a
patelmm79 <patelmm79@gmail.com>
2026-04-02 01:17:58 +0000

    fix: add project to google_compute_router and google_compute_router_nat

--- terraform/postgres_module.tf
+++ terraform/postgres_module.tf
@@ -121,6 +121,7 @@ resource "google_compute_firewall" "allow_egress" {
 
 resource "google_compute_router" "postgres_router" {
   project = var.project_id
+  project = var.project_id
   count   = var.enable_cloud_nat ? 1 : 0
   name    = "pg-${var.instance_name}-router"
   region  = var.region
@@ -130,6 +131,7 @@ resource "google_compute_router" "postgres_router" {
 }
 
 resource "google_compute_router_nat" "postgres_nat" {
+  project = var.project_id
   project = var.project_id
   count  = var.enable_cloud_nat ? 1 : 0
   name   = "pg-${var.instance_name}-nat"
